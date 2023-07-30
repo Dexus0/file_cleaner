2f00bb69a275938344533485a002326be997272b
Dexus <vriendduncan@gmail.com>
2023-07-30 03:03:13 +0200

    Cargo fmt I did the macro by hand.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use nohash_hasher::IntMap as HashMap;
 use std::fs::{read_dir, remove_file};
 use std::io::{Error, ErrorKind};
-use std::path::{Path, PathBuf};
 use std::mem::MaybeUninit;
-use nohash_hasher::IntMap as HashMap;
+use std::path::{Path, PathBuf};
 
 static mut DELETED: usize = 0;
 static mut SCANNED: usize = 0;
@@ -16,15 +16,14 @@ macro_rules! retry_interrupts {
                 Ok(x) => break Ok(x),
                 Err(err) => match err.kind() {
                     ErrorKind::Interrupted => continue, // Try $e again, if error == interrupted
-                    _ => break Err(err)
+                    _ => break Err(err),
                 },
+            }
         }
-    }
     };
 }
 fn main() -> Result<(), Error> {
-    let dir = std::env::args_os()
-        .nth(1).unwrap_or_default();
+    let dir = std::env::args_os().nth(1).unwrap_or_default();
     let paths = read_dir(dir)?.flatten().map(|x| x.path());
 
     remove_duplicates(paths);
@@ -33,19 +32,19 @@ fn main() -> Result<(), Error> {
 }
 
 fn remove_duplicates(paths: impl Iterator<Item = PathBuf>) {
-    unsafe{
+    unsafe {
         MAP = MaybeUninit::new(map_from_iter(&paths)); // initialize MAP before before asynchronous code
         print_scanned(SCANNED); // prints 0 for when no files were found
     }
     for cur_path in paths {
         scan_file(cur_path);
-        unsafe{
-        SCANNED += 1;
-        print_scanned(SCANNED);
+        unsafe {
+            SCANNED += 1;
+            print_scanned(SCANNED);
         }
     }
 
-    unsafe{
+    unsafe {
         println!("\ndeleted: {}", DELETED);
     }
 }
@@ -61,7 +60,7 @@ fn map_from_iter<K, V>(iter: &impl Iterator) -> HashMap<K, V> {
 
     let num = match tuple.1 {
         Some(size) => size,
-        None => tuple.0
+        None => tuple.0,
     };
 
     HashMap::with_capacity_and_hasher(num, BuildNoHashHasher::default())
@@ -73,7 +72,7 @@ fn read_usize<P: AsRef<Path>>(path: P) -> Result<usize, Error> {
 
     match retry_interrupts!(File::open(&path))?.read_exact(&mut buf) {
         Ok(_) => Ok(usize::from_ne_bytes(buf)),
-        Err(e) => Err(e) 
+        Err(e) => Err(e),
     }
 }
 
@@ -86,7 +85,7 @@ fn scan_file(cur_path: PathBuf) {
     let Ok(id) = read_usize(&cur_path) else {
         return;
     };
-            
+
     let Some(paths) = (unsafe{MAP.assume_init_mut().get_mut(&id)}) else {
         unsafe{ MAP.assume_init_mut().insert(id, vec![cur_path]);}
         return;
@@ -107,8 +106,8 @@ fn scan_file(cur_path: PathBuf) {
 
         if other == data {
             match remove_file(cur_path) {
-                Ok(_) => unsafe{DELETED += 1},
-                Err(err) => eprintln!("{err}")
+                Ok(_) => unsafe { DELETED += 1 },
+                Err(err) => eprintln!("{err}"),
             };
             return;
         }
