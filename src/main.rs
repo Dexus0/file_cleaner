40cd642b1a62150ccce10d1e339cd1aea9879a69
Dexus <vriendduncan@gmail.com>
2023-10-06 14:56:06 +0200

    + License

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 use nohash_hasher::IntMap as HashMap;
 use std::fs::{read, read_dir, remove_file};
 use std::io::{Error, ErrorKind};
